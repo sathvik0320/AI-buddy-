41c0b28ed22bcca657db2e8e88a0e0baeb80b147
sathvik0320 <120362787+sathvik0320@users.noreply.github.com>
2024-01-01 23:02:08 +0530

    Update lwr.py

--- lwr_regression/lwr.py
+++ lwr_regression/lwr.py
@@ -1,1 +1,24 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import  LinearRegression 
 
+
+a = pd.read_csv("tips.csv")
+x = a.["tip"]
+y = a.["total_bill"] 
+
+x_train,x_test,y_train,y_test =train_test_split(x,y,random_state=0)
+
+model = LinearRegression()
+
+def weights(x_train,point,tau):
+
+  M = x_train.shape[0]
+  w = np.mat(np.eye(M))
+   
+
+
+model.fit(x_train,y_train,weights)
+plt.scatter(x_test,predict())
